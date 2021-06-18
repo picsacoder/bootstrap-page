09eebb096085a95b0e3b6a1a7724f895d31958a0
picsacoder <66335345+picsacoder@users.noreply.github.com>
2021-06-18 12:07:56 -0500

    AlFinOwO

--- index.js
+++ index.js
@@ -86,14 +86,3 @@ function FuncionInutil(event) {
  }
 
 
- function iniciarMap(){
-    var coord = {lat:-34.5956145 ,lng: -58.4431949};
-    var map = new google.maps.Map(document.getElementById('map'),{
-      zoom: 10,
-      center: coord
-    });
-    var marker = new google.maps.Marker({
-      position: coord,
-      map: map
-    });
-}
