2d9de084ebc39bbd1900d7efcd405b4f0b0b480e
picsacoder <66335345+picsacoder@users.noreply.github.com>
2021-06-18 11:42:17 -0500

    test

--- index.js
+++ index.js
@@ -70,3 +70,16 @@ function FuncionInutil(event) {
     }
     
  }
+
+
+ function iniciarMap(){
+    var coord = {lat:25 ,lng: -107.5};
+    var map = new google.maps.Map(document.getElementById('map'),{
+      zoom: 10,
+      center: coord
+    });
+    var marker = new google.maps.Marker({
+      position: coord,
+      map: map
+    });
+}
